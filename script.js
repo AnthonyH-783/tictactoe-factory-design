1ecb7f1a8190a5f9f18bce2dd58e79e6acf0878c
Anthony <anthonnhani@gmail.com>
2025-11-20 15:39:08 -0700

    Implemented the initial game state

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-// Creating Objects
-
-const gameBoard = (function() {
-
-    const first_player = createPlayer("X");
-    const second_player = createPlayer("O");
-
-    function startGame(){
-
+// Init Module
+const init = (function (){
+    // Initial Data
+    const DIMENSION = 3;
+    let scores = {
+        player_score: 0,
+        comp_score: 0,
+        num_ties: 0,
+    };
+    let X = {
+        dimension: 20,
+        color: "#000000",
+        path: "m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z",
     }
+        let O = {
+        dimension: 20,
+        color: "#000000",
+        path: "M480.28-96Q401-96 331-126t-122.5-82.5Q156-261 126-330.96t-30-149.5Q96-560 126-629.5q30-69.5 82.5-122T330.96-834q69.96-30 149.5-30t149.04 30q69.5 30 122 82.5T834-629.28q30 69.73 30 149Q864-401 834-331t-82.5 122.5Q699-156 629.28-126q-69.73 30-149 30Zm-.28-72q130 0 221-91t91-221q0-130-91-221t-221-91q-130 0-221 91t-91 221q0 130 91 221t221 91Zm0-312Z",
+    }
+    let turn = 0;
 
+    // Caching the DOM
+    const gameboard = document.getElementById("gameboard");
+    const grid = gameboard.querySelector("#middle-rows");
+    const bottom_row = gameboard.querySelector("#bottom-row");
+    const turn_wrapper = gameboard.querySelector("#turn-wrapper");
+    const turn_template = gameboard.querySelector("#turn-template").innerHTML;
+    const scores_template = bottom_row.querySelector("#scores-template").innerHTML;
 
+    // Initial rendering
+    turn_wrapper.innerHTML = Mustache.render(turn_template, X);
+    bottom_row.innerHTML = Mustache.render(scores_template, scores);
+    
+    // Initializing grid item dataset of rows and columns
+    (function (){
+        const grid_items = grid.children;
+        let child_index = 0;
+        for(let row = 0; row < DIMENSION; row++){
+            for(let col = 0; col < DIMENSION; col++){
+                grid_items[child_index].dataset.row = row;
+                grid_items[child_index].dataset.col = col;
+                child_index++;
+            }
+        }
 
-}) ();
+    })();
 
+})();
+
+// Turn Module
+// Score module
 const createPlayer = function(id){
     // Counter for number of times a particular row/col/diagonal is used
     // [row0, row1, row2, col0, col1, col2, diag1, diag2]
     // When one of the above reaches 3, we have a winning condition
     const personal_counter = new Array(8).fill(0); 
+    let score = 0; 
 
     function move(row, col){
         // Row and Column counter automatically incremented
@@ -32,6 +69,7 @@ const createPlayer = function(id){
         }
     }
     function isWinner(){
+        score += 1;
         return personal_counter.includes(3);
     }
     function reset(){
@@ -39,4 +77,45 @@ const createPlayer = function(id){
     }
         
     return {id, move, isWinner, reset};
-}
+};
+// Gameboard Module
+const gameBoard = (function () {
+
+    const first_player = createPlayer("X");
+    const second_player = createPlayer("O");
+    const GRID_SIZE = 3;
+    const board = createGameBoard();
+
+    
+    function createGameBoard(){
+        const list = new Array(GRID_SIZE).fill(new Array(GRID_SIZE).fill(" "));
+        return list;
+    }
+    function reset(){
+        if(!board){
+            board = createGameBoard();
+        }
+        else{
+            for(let row of board){
+                row.fill(" ");
+            }
+        }
+        first_player.reset();
+        second_player.reset();
+    }
+
+    function start(){
+
+        let move_num = 0;
+        while(move_num < 9){
+            let player = (move_num % 2 === 0) ? first_player : second_player;
+            
+
+        }
+
+    }
+
+
+
+})();
+
